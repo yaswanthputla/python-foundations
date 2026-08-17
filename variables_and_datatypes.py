8d04f59088d88d624b6e476bf56d3ce1646fcb97
yaswanthputla <yaswanthputla@gmail.com>
2026-08-17 20:45:57 +0530

    Add variables and data types example

---
+++ variables_and_datatypes.py
@@ -0,0 +1,16 @@
+# Variables and Data Types
+
+name = "Yaswanth"
+age = 17
+height = 5.7
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
